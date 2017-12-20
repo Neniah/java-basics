a7e58dc0b5140e0d12129c6b89eba6934add9710
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-12-20 18:04:30 +0100

    control statement

---
+++ ControlStatement/src/ControStatement.java
@@ -0,0 +1,22 @@
+
+public class ControStatement {
+	
+	public static void main(String[] args) { //Ejemplo If
+		int x = 10;
+		if (x < 20) {
+		System.out.print("X menos que 20\n");
+		}
+		
+		//Ejemplo If/Else If/Else
+        int x = 10;
+        if (x == 10) {
+            System.out.print("X igual a 10\n");
+        } else if (x == 20) {
+            System.out.print("X igual a 20\n");
+        } else if (x == 30) {
+            System.out.print("X igual a 30\n");
+        } else {
+            System.out.print("X no es igual ni a 10, ni 20 ni 30\n");
+        }
+	}
+}
