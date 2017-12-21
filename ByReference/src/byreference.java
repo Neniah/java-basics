3259f8d10d9823e9e15c82a22a8457e091df3727
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-12-21 12:19:58 +0100

    by reference

--- ByReference/src/byreference.java
+++ ByReference/src/byreference.java
@@ -1,4 +1,20 @@
 
-public class byreference {
 
+public class byreference {
+	
+	public static void main(String[] args) {
+        persona p = new persona();
+        p.cambiarNombre("Juan");
+        imprimirNombre(p);//Imprime Juan
+        modificarPersona(p);
+        imprimirNombre(p);//Imprime Carlos
+    }
+
+    public static void modificarPersona(persona arg){
+        arg.cambiarNombre("Carlos");
+    }
+    
+    public static void imprimirNombre(persona p ){
+        System.out.println("Valor recibido :" + p.obtenerNombre());
+    }
 }
---
+++ ByReference/src/persona.java
@@ -0,0 +1,12 @@
+
+public class persona {
+    String nombre;
+    
+    public void cambiarNombre(String nuevoNombre){
+        this.nombre = nuevoNombre;
+    }
+    
+    public String obtenerNombre(){
+        return nombre;
+    }
+}
